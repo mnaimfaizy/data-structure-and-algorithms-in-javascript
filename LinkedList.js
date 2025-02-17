fab28f122f965a8da7ca43a8e9ebae1492bbb662
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-18 10:08:32 +1100

    chore: implement the pop method on LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -24,9 +24,31 @@ class LinkedList {
       this.length++;
     }
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+      this.length--;
+    } else {
+      let newTail = current;
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+    }
+
+    return current;
+  }
 }
 
 const list = new LinkedList();
+/* ----------------- Test push method ----------------- */
 console.log("Initial list:", list);
 list.push(1);
 console.log("List after push(1):", list);
@@ -36,3 +58,13 @@ list.push(3);
 list.push(4);
 list.push(5);
 console.log("List after push(3, 4, 5):", list);
+
+/* ----------------- Test pop method ----------------- */
+list.pop();
+console.log("List after pop():", list);
+list.pop();
+list.pop();
+list.pop();
+console.log("List after pop() 3 times:", list);
+list.pop();
+console.log("List after pop() 1 more time:", list);
