2dc473482c5d3c3b709d8215db029162ef7b364a
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-19 18:18:00 +1100

    feat: add unshift method to DoublyLinkedList class

--- test.js
+++ test.js
@@ -6,11 +6,5 @@ console.log(myDoublyLinkedList);
 myDoublyLinkedList.push(20);
 console.log(myDoublyLinkedList);
 
-myDoublyLinkedList.pop();
-console.log("After 1 time pop: ", myDoublyLinkedList);
-
-myDoublyLinkedList.pop();
-console.log("After 2 times pop: ", myDoublyLinkedList);
-
-myDoublyLinkedList.pop();
-console.log("If we have no elements: ", myDoublyLinkedList);
+myDoublyLinkedList.unshift(5);
+console.log("Unshifted:", myDoublyLinkedList);
