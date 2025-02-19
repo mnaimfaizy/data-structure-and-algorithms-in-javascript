b85a01be4f51ad9060482a65ef1ae434d7a317ef
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-19 14:00:02 +1100

    feat: add pop method to DoublyLinkedList class

--- test.js
+++ test.js
@@ -5,3 +5,12 @@ console.log(myDoublyLinkedList);
 
 myDoublyLinkedList.push(20);
 console.log(myDoublyLinkedList);
+
+myDoublyLinkedList.pop();
+console.log("After 1 time pop: ", myDoublyLinkedList);
+
+myDoublyLinkedList.pop();
+console.log("After 2 times pop: ", myDoublyLinkedList);
+
+myDoublyLinkedList.pop();
+console.log("If we have no elements: ", myDoublyLinkedList);
