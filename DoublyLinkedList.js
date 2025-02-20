7a42fed7a08397434f675c687f69542947a210eb
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-21 05:54:54 +1100

    feat: add insert method to DoublyLinkedList class

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -96,6 +96,22 @@ class DoublyLinkedList {
     }
     return false;
   }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.push(value);
+
+    const newNode = new Node(value);
+    const beforeNode = this.get(index - 1);
+    const afterNode = beforeNode.next;
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
 }
 
 module.exports = DoublyLinkedList;
--- test.js
+++ test.js
@@ -10,3 +10,9 @@ console.log("Get the 3 element: ", myDoublyLinkedList.get(2).value);
 
 myDoublyLinkedList.set(1, 10);
 console.log("Get the 2 element after set: ", myDoublyLinkedList.get(1).value);
+
+myDoublyLinkedList.insert(2, 20);
+console.log(
+  "Get the 3 element after insert: ",
+  myDoublyLinkedList.get(2).value
+);
