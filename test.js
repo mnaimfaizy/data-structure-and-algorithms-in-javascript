b9bdbb8c9f74226520f980c539a6d84477add870
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-22 16:35:01 +1100

    feat: implement Stack class with push and pop methods

--- test.js
+++ test.js
@@ -1,22 +1,11 @@
-const DoublyLinkedList = require("./DoublyLinkedList");
+const Stack = require("./Stack.js");
 
-const myDoublyLinkedList = new DoublyLinkedList(0);
-myDoublyLinkedList.push(1);
-myDoublyLinkedList.push(2);
-myDoublyLinkedList.push(3);
+const stack = new Stack(1);
+stack.push(2);
+stack.push(3);
+console.log(stack);
 
-console.log("Get the 2 element: ", myDoublyLinkedList.get(1).value);
-console.log("Get the 3 element: ", myDoublyLinkedList.get(2).value);
-
-myDoublyLinkedList.set(1, 10);
-console.log("Get the 2 element after set: ", myDoublyLinkedList.get(1).value);
-
-myDoublyLinkedList.insert(2, 20);
-console.log(
-  "Get the 3 element after insert: ",
-  myDoublyLinkedList.get(2).value
-);
-
-console.log("List before removing the item: ", myDoublyLinkedList);
-myDoublyLinkedList.remove(1);
-console.log("Remove the item in index 1: ", myDoublyLinkedList);
+stack.pop();
+console.log(stack);
+stack.pop();
+console.log(stack);
