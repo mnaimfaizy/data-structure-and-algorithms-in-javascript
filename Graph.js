2bb20becd86ff87cda44f77c6b6bf4b78033a598
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-23 19:12:08 +1100

    feat: add removeEdge and removeVertex methods to Graph class

--- Graph.js
+++ Graph.js
@@ -19,6 +19,29 @@ class Graph {
     }
     return false;
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+      return true;
+    }
+    return false;
+  }
+
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return undefined;
+    while (this.adjacencyList[vertex].length) {
+      const temp = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, temp);
+    }
+    delete this.adjacencyList[vertex];
+    return this;
+  }
 }
 
 module.exports = Graph;
--- test.js
+++ test.js
@@ -8,6 +8,16 @@ g.addVertex("A");
 console.log("Adjacency list with vertex A:", g);
 
 g.addVertex("B");
+g.addVertex("C");
 
 g.addEdge("A", "B");
 console.log("Adjacency list with edge A-B:", g);
+g.addEdge("A", "C");
+g.addEdge("B", "C");
+console.log("Adjacency list with edges A-B and A-C and B-C:", g);
+
+g.removeEdge("A", "B");
+console.log("Adjacency list with edge A-B removed:", g);
+
+g.removeVertex("B");
+console.log("Adjacency list with vertex B removed:", g);
