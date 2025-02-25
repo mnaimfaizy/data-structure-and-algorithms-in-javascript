c466102e7e322efdadad77c45eecafccc9abf39a
Mohammad Naim Faizy <mnaim.faizy@gmail.com>
2025-02-26 06:27:43 +1100

    feat: implement Bubble Sort algorithm in new BubbleSort.js file

---
+++ BubbleSort.js
@@ -0,0 +1,14 @@
+function bubbleSort(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([5, 3, 8, 2, 1, 4])); // [1, 2, 3, 4, 5, 8]
